a540fb175ffb9bcc79a2e5a4676744658170d540
isotop786 <marufislam77@gmail.com>
2022-09-26 07:13:19 +0600

    wrapped up

--- src/main/java/com/example/demo/web/HomeController.java
+++ src/main/java/com/example/demo/web/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.domain.User;
 
@@ -19,8 +20,16 @@ public class HomeController {
 		}
 		
 		@RequestMapping(value ="/create", method=RequestMethod.POST)
-		public String processData(User user)
+		public String processData(User user, RedirectAttributes reAttr)
 		{	
-			return "responsetest";
+			reAttr.addFlashAttribute("user", user);
+//			return "responsetest";
+			return "redirect:/display";
+		}
+		
+		@RequestMapping(value="/display", method = RequestMethod.GET)
+		public String displayFormData(User user)
+		{
+			return "responsetest"; 
 		}
 }
